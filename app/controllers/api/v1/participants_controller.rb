399090b63bd4db1e7f95b06d2656c00c886cd77e
Hien Nguyen <quochien@gmail.com>
2019-06-15 15:50:07 +0700

    create, update participant

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -6,15 +6,38 @@ module Api::V1
     end
 
     def create
+      participant = Participant.new(participant_params)
+
+      if participant.save
+        render json: participant
+      else
+        render json: { error: participant.errors.full_messages }
+      end
     end
 
     def update
+      unless Participant.statuses.keys.include?(params[:status])
+        render json: { error: 'Invalid status' }
+        return
+      end
+
+      if participant = Participant.find_by_id(params[:id])
+        if participant.pending?
+          participant.status = params[:status]
+          participant.save
+          render json: participant
+        else
+          render json: { error: "Participant has already been #{participant.status}" }
+        end
+      else
+        render json: { error: 'Participant not found' }
+      end
     end
 
     private
 
     def participant_params
-      params.require(:participant).permit(:f_name, :l_name, :phone, :email, :gender, :current_weight, :height, :zip)
+      params.permit(:f_name, :l_name, :phone, :email, :gender, :current_weight, :height, :zip)
     end
 
   end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,5 @@
 class Participant < ApplicationRecord
   enum gender: { not_given: 0, male: 1, female: 2 }
   enum status: { pending: 0, accepted: 1, rejected: 2 }
+  validates :email, uniqueness: { case_sensitive: false }, presence: false
 end
